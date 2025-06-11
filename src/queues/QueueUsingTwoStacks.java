890b122d7c989a3d8eeecdd2efde363d4c992210
Catalin Csibi <csibicatalin@gmail.com>
2025-06-11 12:27:56 +0300

    modified queue using 2 stacks

--- src/queues/QueueUsingTwoStacks.java
+++ src/queues/QueueUsingTwoStacks.java
@@ -4,30 +4,30 @@ import java.util.Stack;
 
 public class QueueUsingTwoStacks <E> {
 
-    private Stack<E> stack1 = new Stack<>();
-    private Stack<E> stack2 = new Stack<>();
+   private Stack<E> stack1 = new Stack<>();
+   private Stack<E> stack2 = new Stack<>();
 
-    public void enqueue(E item) {
-        stack1.push(item);
-    }
+   public void enqueue(E item) {
+       stack1.push(item);
+   }
 
-    public E dequeue() {
-        if(isEmpty())
-            throw new IllegalStateException();
+   public E dequeue() {
+       moveStack1ToStack2();
+       return stack2.pop();
+   }
 
-        if(stack2.isEmpty())
-            moveStack1ToStack2();
-
-        return stack2.pop();
-    }
+   public E peek() {
+       moveStack1ToStack2();
+       return stack2.peek();
+   }
 
     private void moveStack1ToStack2() {
-        while (!stack1.isEmpty())
-            stack2.push(stack1.pop());
+        if(stack2.isEmpty()) {
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
     }
 
-    public boolean isEmpty(){
-        return stack1.isEmpty() && stack2.isEmpty();
-    }
 
 }
