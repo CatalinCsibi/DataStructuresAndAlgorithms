2a769511eab1bc70b8e20c6d5cf4d6aa524cb6a0
Catalin Csibi <csibicatalin@gmail.com>
2025-06-17 21:26:13 +0300

    added bubble sort for strings

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -11,6 +11,14 @@ public class BubbleSort {
             System.out.print(i + " ");
         }
         System.out.println();
+
+        String[] strings = {"Hello", "Bye", "Ciao", "Goodnight", "Goodbye", };
+        bubbleSortStrings(strings);
+
+        for(String s : strings) {
+            System.out.print(s + " ");
+        }
+
     }
 
     public static void bubbleSort(int[] array) {
@@ -23,6 +31,18 @@ public class BubbleSort {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void bubbleSortStrings(String[] array) {
+        for(int i =0; i< array.length; i++) {
+            for(int j = 0; j < array.length-1; j++) {
+                if(array[j].compareTo(array[j+1]) > 0) {
+                    String temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
                 }
             }
         }
