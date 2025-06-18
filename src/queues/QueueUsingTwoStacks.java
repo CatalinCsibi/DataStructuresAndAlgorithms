94db4847114cb969e8c3c2ef15414ca06dbe37dd
Catalin Csibi <csibicatalin@gmail.com>
2025-06-18 13:16:40 +0300

    modied queue using 2 stacks

--- src/queues/QueueUsingTwoStacks.java
+++ src/queues/QueueUsingTwoStacks.java
@@ -4,26 +4,21 @@ import java.util.Stack;
 
 public class QueueUsingTwoStacks <E> {
 
-   private Stack<E> stack1 = new Stack<>();
-   private Stack<E> stack2 = new Stack<>();
+    private Stack<E> stack1 = new Stack<>();
+    private Stack<E> stack2 = new Stack<>();
 
-   public void enqueue(E item) {
-       stack1.push(item);
-   }
+    public void enqueue(E item) {
+        stack1.push(item);
+    }
 
-   public E dequeue() {
-       moveStack1ToStack2();
-       return stack2.pop();
-   }
-
-   public E peek() {
-       moveStack1ToStack2();
-       return stack2.peek();
-   }
+    public E dequeue() {
+        moveStack1ToStack2();
+        return stack2.pop();
+    }
 
     private void moveStack1ToStack2() {
         if(stack2.isEmpty()) {
-            while (!stack1.isEmpty()){
+            while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
