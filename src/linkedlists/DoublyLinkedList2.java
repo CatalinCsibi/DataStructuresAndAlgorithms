6b68a3ac6d64910dad654a8c8d8eb9bd08ee07f2
Catalin Csibi <csibicatalin@gmail.com>
2025-06-20 14:19:21 +0300

    added main method to DoublyLinkedList2

--- src/linkedlists/DoublyLinkedList2.java
+++ src/linkedlists/DoublyLinkedList2.java
@@ -156,5 +156,33 @@ public class DoublyLinkedList2 <V> {
         System.out.println();
     }
 
+    public static void main(String[] args) {
+
+        DoublyLinkedList2<String> doublyLinkedList2 = new DoublyLinkedList2<>();
+
+        doublyLinkedList2.addFirst("Hello");
+        doublyLinkedList2.addFirst("Salut");
+        doublyLinkedList2.addLast("Bye");
+        doublyLinkedList2.addLast("Ciao");
+
+        System.out.println(doublyLinkedList2.size());
+        System.out.println(doublyLinkedList2.contains("Ciao"));
+        System.out.println(doublyLinkedList2.getFirst());
+        System.out.println(doublyLinkedList2.getLast());
+
+        doublyLinkedList2.removeFirst();
+        doublyLinkedList2.removeLast();
+
+        doublyLinkedList2.remove("Bye");
+
+        doublyLinkedList2.addFirst("Salut");
+
+        doublyLinkedList2.printLinkedList();
+
+        doublyLinkedList2.reverseLinkedList();
+
+        doublyLinkedList2.printLinkedList();
+    }
+
 
 }
