76b3d63893908a7fc704fdb73de11224375e57b2
Catalin Csibi <csibicatalin@gmail.com>
2025-06-25 15:46:38 +0200

    MajorityElement added algorithm

--- src/algorithms/exercies/MajorityElement.java
+++ src/algorithms/exercies/MajorityElement.java
@@ -1,8 +1,5 @@
 package algorithms.exercies;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MajorityElement {
 
 /*  Majority Element
@@ -10,8 +7,6 @@ public class MajorityElement {
     Given an array nums of size n, return the majority element.
 
     The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
-
-
 
     Example 1:
 
