ad9e25ad2db58ed6c131ea30cab924a9dff97116
Catalin Csibi <csibicatalin@gmail.com>
2025-06-26 10:59:52 +0200

    modified FactorialExample , corrected logic error

--- src/algorithms/recursion/FactorialExample.java
+++ src/algorithms/recursion/FactorialExample.java
@@ -9,16 +9,17 @@ public class FactorialExample {
 
 
     private static int factorialRecursive(int number) {
-        if(number == 2)
-            return 2;
+        if(number < 2)
+            return 1;
 
         return number * factorialRecursive(number -1);
     }
 
     private static int factorialIterative(int number) {
-        for(int i = number-1; i > 0; i--) {
-            number = number * i;
-        }
-        return number;
+       int result = 1;
+       for(int i = 2; i <= number; i++) {
+           result *= i;
+       }
+       return result;
     }
 }
