8e780878e77db97f3d0b54a0679762339a2730a6
Catalin Csibi <csibicatalin@gmail.com>
2025-07-03 09:08:49 +0200

    improved performance of bubble sort string

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -37,14 +37,18 @@ public class BubbleSort {
     }
 
     private static void bubbleSortStrings(String[] array) {
-        for(int i =0; i< array.length; i++) {
+        for(int i = 0; i < array.length; i++) {
+            boolean swapped = false;
             for(int j = 0; j < array.length-1; j++) {
                 if(array[j].compareTo(array[j+1]) > 0) {
                     String temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
+                    swapped = true;
                 }
             }
+            if(!swapped)
+                break;
         }
     }
 
